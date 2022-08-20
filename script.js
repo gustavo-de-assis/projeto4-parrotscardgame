5f627a32d2614e254cbc23b59734af86dde8943b
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-08-20 19:17:17 -0300

    insereCartas() implementada

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let qtdCartas;
 
 function virarCarta(elemento){
     elemento.classList.add("carta-virada");
 
 }
+
+function insereCartas(){
+    qtdCartas = Number(prompt("Com quantas cartas voce gostaria de jogar? (numero par entre 4 e 14)"));
+
+    if(qtdCartas%2 === 1 || qtdCartas > 14 || qtdCartas < 4){
+        alert('Valor invalido! favor entrar com numero par entre 4 e 14.');
+        insereCartas(qtdCartas);
+    }
+
+}
+
+insereCartas();
+console.log(qtdCartas);
